97cf9e0f1e66d3a3a85fa8350e2dcbaa4c939dc3
FelipeFelixHP <106828596+FelipeFelixHP@users.noreply.github.com>
2022-09-06 23:40:04 -0300

    Add files via upload
    Conversor de moedas
    Descubra os valores em dolar U$ e Bitcoin ₿

---
+++ interaçaoconversor.js
@@ -0,0 +1,25 @@
+function ConverterDolar(){
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value; 
+    var valorEmDolarNumerico = Number(valor);
+
+    var valorEmReal = valorEmDolarNumerico * 5.25;
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+
+    var valorConvertido = ` O resuldado em Dolar U$ é  ${valorEmReal}`
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+function ConverterBTC(){
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value; 
+    var valorEmBtcNumerico = Number(valor);
+
+    var valorEmReal = valorEmBtcNumerico * 0.000010;
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+
+    var valorConvertido = ` O resuldado em Bitcoin é ₿ ${valorEmReal}`
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
